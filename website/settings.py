938ab622c332bfd3b2e54c37798fe70a65f25c9d
EZ <dw04@ya.ru>
2023-05-06 18:47:59 +0300

    init

--- website/settings.py
+++ website/settings.py
@@ -75,9 +75,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_orm_migrations',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
-        'USER': 'postgres',
-        'PASSWORD': 'Zxcvbnm0+'
+        'PORT': '5432'
     }
 }
 
